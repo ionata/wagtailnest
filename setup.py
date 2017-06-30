f7fd4b701726ca11d5d71c3a3d41dc755066dd81
Chris Darko <chris@ionata.com.au>
2017-06-30 14:43:58 +1000

    Remove requirements.txt, local-dev requirements

--- setup.py
+++ setup.py
@@ -11,12 +11,6 @@ with open('README.rst', 'r') as f:
 
 requirements = []
 
-with io.open('requirements.txt', encoding='utf-8') as fyle:
-    for line in fyle.readlines():
-        parsed = line.strip('\n').strip()
-        if line != '' and line[0] != '#':
-            requirements.append(line)
-
 
 setup(
     name='wagtailnest',
@@ -27,7 +21,42 @@ setup(
     author_email='webmaster@ionata.com.au',
     url='https://github.com/ionata/wagtailnest',
     packages=find_packages('src'),
-    install_requires=requirements,
+    install_requires=[
+        ## Setup/running requirements
+        'django-environ~=0.4.3',
+
+        ## Core requirements
+        'django~=1.11.0',
+        'djangorestframework~=3.6.2',
+        'djangorestframework-filters~=0.10.0',
+        'djangorestframework-jwt',
+        'django-allauth~=0.27.0',
+        'django-cors-headers~=1.2.2',
+        'django-csv-utils',
+        'django-enumfields',
+        'django-filter~=1.0.2',
+        'django-minimal-user==0.0.1',
+        'django-rest-auth~=0.9.0',
+        'django-rest-swagger~=2.0.6',
+        'django-revproxy==0.9.13',
+        'wagtail~=1.10.0',
+        'django-anymail[mailgun]~=0.5.0',
+        'django-extensions',
+
+        ## Tasking
+        'celery[redis]',
+
+        ## Python extras
+        'pytz',
+        'python-dateutil',
+        'Pillow',
+        'requests==2.15.1',
+        'geopy',
+        'faker',
+        'pygments',
+        'markdown',
+        'beautifulsoup4',
+    ],
     package_dir={'': 'src'},
     include_package_data=True,
     classifiers=[
